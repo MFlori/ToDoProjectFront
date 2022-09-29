272ad623ec60a6679359be3ba58ec708cdae4942
MFlori <Slldrgi9>
2022-09-29 08:57:40 +0200

    username and pw input is now required for login

--- scriptLogin.js
+++ scriptLogin.js
@@ -1,23 +1,27 @@
 const userNameInput = document.getElementById("userName");
 const passwordInput = document.getElementById("password");
 const btnLogin = document.getElementById("btnLogin");
+const errorMessage = document.getElementById("errorMessage");
 
 btnLogin.addEventListener("click", () => {
-  const userID = getUserID(
-    "http://localhost:8080/getUserID/" +
-      userNameInput.value +
-      "/" +
-      passwordInput.value
-  );
+  if (userNameInput.value !== "" && passwordInput.value !== "") {
+    const userID = getUserID(
+      "http://localhost:8080/getUserID/" +
+        userNameInput.value +
+        "/" +
+        passwordInput.value
+    );
+  } else {
+    errorMessage.innerHTML = "Benutzername UND Passwort eingeben</br>";
+  }
 });
 
 async function getUserID(url) {
   const response = await fetch(url);
   let data = await response.text();
   if (data !== "") {
-    console.log(data);
     document.location = "home.html?user=" + data;
   } else {
-    console.log("error");
+    errorMessage.innerHTML = "Passwort ungültig oder User nicht angelegt";
   }
 }
